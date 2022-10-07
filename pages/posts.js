1ba1e9a93099768a9c59bcf42458c33ac6c82e42
Egor Kosaretskiy <52824960+Jahysama@users.noreply.github.com>
2022-10-07 17:56:19 +0300

    Changed to publications

--- pages/posts.js
+++ pages/posts.js
@@ -3,24 +3,44 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import OldSite from '../public/images/contents/old_site.jpg'
+import GRAPE from '../public/images/contents/grape.jpg'
+import MUTS from '../public/images/contents/muts.jpg'
+import CONF from '../public/images/contents/conf.jpg'
 
-const Posts = () => (
-  <Layout title="Posts">
+const Publications = () => (
+  <Layout title="Publications">
     <Container>
       <Heading as="h3" fontSize={20} mb={4}>
-        Posts
+        Publications
       </Heading>
 
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <Section>
           <GridItem
-            title="My old terrible website"
-            thumbnail={OldSite}
-            href="https://kodomo.fbb.msu.ru/~kosar/"
+            title="Genomic Relatedness Detection Pipeline"
+            thumbnail={GRAPE}
+            href="https://www.biorxiv.org/content/10.1101/2022.03.11.483988v1"
           />
+         </Section>
+
+          <Section>
+          <GridItem
+            title="A Promising Tool for the Crosslinking of the MutS Protein Preserving Its Functional Activity"
+            thumbnail={MUTS}
+            href="https://link.springer.com/article/10.1134/S1068162021020205"
+          />
+         </Section>
+
+         </Section>
+          <Section>
+          <GridItem
+            title="Computational biology and artificial intelligence for personalized medicine-2022"
+            thumbnail={CONF}
+            href="https://doi.org/10.14341/CBAI-2022-44"
+          />
+         </Section>
         </SimpleGrid>
-      </Section>
     </Container>
   </Layout>
 )
