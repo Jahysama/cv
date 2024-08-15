2600b9daef6c9e1732180917f4bc6e286c3dc29f
Jahysama <megametagross@outlook.de>
2024-08-15 11:30:56 +0500

    Remove elements from rendering on mobile

--- scripts/crow.js
+++ scripts/crow.js
@@ -1,136 +1,149 @@
-const crowFrames = [
-            `
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+if (!isMobileDevice()) {
+
+  const crowFrames = [
+
+    `
       ___     
      (o,o)    
      (   )    
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (-,-)    
      (   )    
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (o,o)    
      (   )    
     -"-"---   
     `,
-            `
+    `
       ___     
      (o,o)    
      (   )    
     ---"-"-   
     `,
-            `
+    `
        __     
       (o,o)   
      >(   )   
     --"-"-"-  
     `,
-            `
+    `
      __       
     (o,o)     
    <(   )<    
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (o,o)    
      (   )v   
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (o,-)    
      (   )    
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (-,o)    
      (   )    
     -"-"-"-   
     `,
-            `
+    `
       ___     
      (o,o)    
      (   )    
     -"-"-"-   
     `
-        ];
+  ];
 
-        const deathFrames = [
-            `
+  const deathFrames = [
+    `
       ___     
      (x,x)    
      (   )    
     -"-"-"-   
     `,
-            `
+    `
       ___    
    ( x , x )   
     (     )   
     -"---"-   
     `,
-            `
+    `
     ___ ___   
   ( x  ,  x )  
    (       )  
     -"---"-   
     `,
-            `
+    `
     ___ ___  
  ( x   ,   x ) 
   (         ) 
     -"---"-   
     `,
-            `
+    `
       ..  ;
     :      .
   :  : :  .
     -"---"-   
     `,
-            `
+    `
 
 
 
     -"-"-"-   
     `
-        ];
+  ];
 
-        let currentFrame = 0;
-        let isAlive = true;
-        let animationInterval;
-        const crowElement = document.getElementById('crow');
+  let currentFrame = 0;
+  let isAlive = true;
+  let animationInterval;
+  const crowElement = document.getElementById('crow');
 
-        function animateCrow() {
-            crowElement.textContent = crowFrames[currentFrame];
-            currentFrame = (currentFrame + 1) % crowFrames.length;
-        }
+  function animateCrow() {
+    crowElement.textContent = crowFrames[currentFrame];
+    currentFrame = (currentFrame + 1) % crowFrames.length;
+  }
 
-        function animateDeath() {
-            let deathFrame = 0;
-            clearInterval(animationInterval);
-            
-            const deathInterval = setInterval(() => {
-                if (deathFrame < deathFrames.length) {
-                    crowElement.textContent = deathFrames[deathFrame];
-                    deathFrame++;
-                } else {
-                    clearInterval(deathInterval);
-                }
-            }, 200);
-        }
+  function animateDeath() {
+    let deathFrame = 0;
+    clearInterval(animationInterval);
 
-        crowElement.addEventListener('click', () => {
-            if (isAlive) {
-                isAlive = false;
-                animateDeath();
-            }
-        });
+    const deathInterval = setInterval(() => {
+      if (deathFrame < deathFrames.length) {
+        crowElement.textContent = deathFrames[deathFrame];
+        deathFrame++;
+      } else {
+        clearInterval(deathInterval);
+      }
+    }, 200);
+  }
 
-        animationInterval = setInterval(animateCrow, 200);
+  crowElement.addEventListener('click', () => {
+    if (isAlive) {
+      isAlive = false;
+      animateDeath();
+    }
+  });
 
+  animationInterval = setInterval(animateCrow, 200);
 
+} else {
+  // For mobile devices, remove the crow element entirely
+  const crowElement = document.getElementById('crow');
+  if (crowElement) {
+    crowElement.remove();
+  }
+}
