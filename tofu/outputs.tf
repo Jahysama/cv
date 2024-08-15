ed703902a2ec34fc6de6dd8298fa7c7f3229f3f5
Jahysama <megametagross@outlook.de>
2024-08-15 16:14:57 +0500

    remove outputs

--- tofu/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "website_url" {
-  description = "The external IP address of the website instance"
-  value       = google_compute_instance.website_instance.network_interface[0].access_config[0].nat_ip
-}
