16624514c0f725f13c9ecba5e7d3141936127b83
Jahysama <megametagross@outlook.de>
2025-01-02 01:03:03 +0500

    add flake dev env

--- flake.nix
+++ flake.nix
@@ -1,31 +1,38 @@
 {
-  description = "Construct development shell from requirements.txt";
+ inputs = {
+   nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+   devenv.url = "github:cachix/devenv";
+   nixpkgs-python.url = "github:cachix/nixpkgs-python";
+   nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
+ };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+ nixConfig = {
+   extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+   extra-substituters = "https://devenv.cachix.org";
+ };
 
-  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+ outputs = { self, nixpkgs, devenv, ... } @ inputs:
+   let
+     system = "x86_64-linux";
+     pkgs = nixpkgs.legacyPackages.${system};
+   in
+   {
+     packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
 
-  outputs =
-    { nixpkgs, pyproject-nix, ... }:
-    let
-      # Load/parse requirements.txt
-      project = pyproject-nix.lib.project.loadRequirementsTxt { projectRoot = ./.; };
-
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      python = pkgs.python3;
-
-      pythonEnv =
-        # Assert that versions from nixpkgs matches what's described in requirements.txt
-        # In projects that are overly strict about pinning it might be best to remove this assertion entirely.
-        assert project.validators.validateVersionConstraints { inherit python; } == { };
-        (
-          # Render requirements.txt into a Python withPackages environment
-          pkgs.python3.withPackages (project.renderers.withPackages { inherit python; })
-        );
-
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell { packages = [ pythonEnv ]; };
-    };
+     devShells.${system}.default = devenv.lib.mkShell {
+       inherit inputs pkgs;
+       modules = [
+         {
+           languages.python = {
+             enable = true;
+             version = "3.11";
+             venv = {
+               enable = true;
+               requirements = ./requirements.txt;
+             };
+           };
+         }
+       ];
+     };
+   };
 }
-
